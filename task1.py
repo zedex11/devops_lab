5b9e18572bb2e5e88cc5ec00819f463aca5ae7a1
Siarhei_Hryshchanka <zedex@tut.by>
2020-09-21 15:25:43 +0000

    python_homework2

--- task1.py
+++ task1.py
@@ -7,19 +7,23 @@ def create_diction():
             values = list(map(int, input("Enter numeric values separate by space: ").split()))
         # error handling
         except ValueError:
-            print("Error! Wrong input, please try again...")
+            print("Error! Wrong input, please input only numbers separate by space...")
 
         else:
-            diction = {}
-            # if key list more than value list, value list equalizes by add value "None" in the end
-            if len(keys) > len(values):
-                for i in range((len(keys) - len(values))):
-                    values.append('None')
-            # fill the dictionary
-            for n in range(len(keys)):
-                diction[keys[n]] = values[n]
-            print(diction)
-            break
+            # check key list for uniqueness of elements
+            if len(keys) == len(set(keys)):
+                diction = {}
+                # if key list more than value list, value list equalizes by add value "None"
+                if len(keys) > len(values):
+                    for i in range((len(keys) - len(values))):
+                        values.append('None')
+                # fill the dictionary
+                for n in range(len(keys)):
+                    diction[keys[n]] = values[n]
+                print(diction)
+                break
+            else:
+                print("keys values must be unique and not repeated")
 
 
 create_diction()
