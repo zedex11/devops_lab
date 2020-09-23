252fb5fba688de233a1fe7a1e3e87fa5b738cabb
Siarhei_Hryshchanka <zedex@tut.by>
2020-09-23 09:57:22 +0000

    python_Homework4

---
+++ script.py
@@ -0,0 +1,14 @@
+import yaml
+from jinja2 import Template
+
+with open('data.yml') as data_file:
+    config_data = yaml.load(data_file)
+
+with open('vhosts.j2') as template_file:
+    template_html = template_file.read()
+
+template = Template(template_html)
+vhosts_conf = template.render(config_data)
+
+with open('vhosts.conf', 'w') as vhosts_file:
+    vhosts_file.write(vhosts_conf)
